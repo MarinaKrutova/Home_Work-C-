5a3c746f40376eb153870f0d13e6656b0ff574d2
Marina <marra@inbox.ru>
2022-11-21 11:05:29 +0300

    Tesk02

---
+++ HomeWork01/Tesk01/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+int userNumberA = new int();
+int userNumberB = new int();
+Console.Write("Введите число 1: ");
+userNumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+userNumberB = Convert.ToInt32(Console.ReadLine());
+if (userNumberA > userNumberB)
+{
+    Console.WriteLine("большее число: " + userNumberA);
+    Console.WriteLine("меньшее число: " + userNumberB);
+}
+else if (userNumberA < userNumberB)
+{
+    Console.WriteLine("большее число: " + userNumberB);
+    Console.WriteLine("меньшее число: " + userNumberA);
+}
+else
+{
+    Console.WriteLine("числа равны");
+}
---
+++ HomeWork01/Tesk02/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+int userNumberA = new int();
+int userNumberB = new int();
+int userNumberC = new int();
+Console.Write("Введите число 1: ");
+userNumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+userNumberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+userNumberC = Convert.ToInt32(Console.ReadLine());
+int max = userNumberA;
+if (userNumberB > max)
+{
+    max = userNumberB;
+}
+if (userNumberC > max)
+{
+    max = userNumberC;
+}
+Console.WriteLine("Максимальное число: " + max);
